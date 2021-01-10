4da02e1f7fa4c2a6db260def2ed651aeb6d2cecc
hoanggiang1123 <hoanggiang1123@gmail.com>
2021-01-10 11:39:00 +0800

    add ck

---
+++ plugins/wysiwyg/admin/src/components/CKEditor/index.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import styled from 'styled-components';
+// import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
+// ClassicEditor.builtinPlugins.push(Alignment);
+// console.log(ClassicEditor.builtinPlugins)
+const Wrapper = styled.div`
+  .ck-editor__main {
+    min-height: 200px;
+    > div {
+      min-height: 200px;
+    }
+  }
+`;
+
+const configuration = {
+    alignment: {
+        options: [ 'left', 'right']
+    },
+  toolbar: [
+    'heading',
+    '|',
+    'bold',
+    'italic',
+    'link',
+    'bulletedList',
+    'numberedList',
+    '|',
+    'indent',
+    'outdent',
+    '|',
+    'blockQuote',
+    'alignment',
+    'insertTable',
+    'undo',
+    'redo',
+  ],
+  heading: {
+    options: [
+    { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+    { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+    { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' },
+    { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' },
+    { model: 'heading4', view: 'h4', title: 'Heading 4', class: 'ck-heading_heading4' },
+    { model: 'heading5', view: 'h5', title: 'Heading 5', class: 'ck-heading_heading5' },
+    { model: 'heading6', view: 'h6', title: 'Heading 6', class: 'ck-heading_heading6' }
+    ]
+ }
+};
+
+const Editor = ({ onChange, name, value }) => {
+  return (
+    <Wrapper>
+      <CKEditor
+        editor={ClassicEditor}
+        config={configuration}
+        data={value}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          onChange({ target: { name, value: data } });
+        }}
+      />
+    </Wrapper>
+  );
+};
+
+Editor.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+};
+
+export default Editor;
